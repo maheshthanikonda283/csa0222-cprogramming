76312b4840fe2b1c7afdf72df0fa121e414a3816
maheshthanikonda283 <144665368+maheshthanikonda283@users.noreply.github.com>
2023-09-11 13:39:55 +0530

    Add files via upload

---
+++ simple interset.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int p,t,r,c;
+	printf("enter the values of p,t,r");
+	scanf("%d%d%d",&p,&t,&r);
+       c=p*t*r/100;
+	printf("%d is simple interest:\n");
+	return 0;
+}
